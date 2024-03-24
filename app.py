8f834c548dfbcfe8f3fb588c61389e50456933e9
Reem <143909386+ReemB4@users.noreply.github.com>
2024-03-24 09:16:47 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,72 +1,236 @@
-from flask import Flask, render_template, request, redirect, url_for
+
+from flask import Flask, render_template, request, make_response, redirect, url_for, flash
+import sqlite3
 
 app = Flask(__name__)
 
-# قائمة المستخدمين الوهميين
-users = [
-    {"id": 1, "username": "doctor1", "password": "password1", "patient": "patient1"},
-    {"id": 2, "username": "doctor2", "password": "password2", "patient": "patient2"},
-    {"id": 3, "username": "patient1", "password": "password3", "doctor": "doctor1"},
-    {"id": 4, "username": "patient2", "password": "password4", "doctor": "doctor2"},
-    {"id": 5, "username": "patient3", "password": "password5", "doctor": "doctor2"},
-    {"id": 6, "username": "patient4", "password": "password6", "doctor": "doctor1"},
-    {"id": 7, "username": "admin", "password": "adminpassword", "role": "admin"}
-]
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-# معلومات مرضى وهمية
-patients_db = {
-    "patient1": {"name": "مريض 1", "age": 30, "gender": "ذكر", "diagnosis": "التهاب الحلق"},
-    "patient2": {"name": "مريض 2", "age": 45, "gender": "أنثى", "diagnosis": "ارتفاع ضغط الدم"},
-    "patient3": {"name": "مريض 3", "age": 55, "gender": "ذكر", "diagnosis": "سكري"},
-    "patient4": {"name": "مريض 4", "age": 28, "gender": "أنثى", "diagnosis": "التهاب المفاصل"}
-}
+# Function to establish connection to SQLite database
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    #conn.row_factory = sqlite3.Row
+    return conn
+
+# Create table if not exists
+def create_table():
+    conn = get_db_connection()
+    #c = conn.cursor()
+    print("Connected to database successfully")
+
+    conn.execute('CREATE TABLE IF NOT EXISTS patients (id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT NOT NULL UNIQUE,password TEXT NOT NULL,score TEXT NOT NULL)')
+    conn.execute('CREATE TABLE IF NOT EXISTS visits (id INTEGER PRIMARY KEY AUTOINCREMENT,patient_id INTEGER NOT NULL,visit_date DATE NOT NULL,FOREIGN KEY (patient_id) REFERENCES users (id))')
+    conn.execute('CREATE TABLE IF NOT EXISTS proteins (id INTEGER PRIMARY KEY AUTOINCREMENT,visit_id INTEGER NOT NULL,protein_name TEXT NOT NULL,NPX TEXT NOT NULL,FOREIGN KEY (visit_id) REFERENCES visits (id))')
+    conn.execute('CREATE TABLE IF NOT EXISTS peptides ( id INTEGER PRIMARY KEY AUTOINCREMENT, visit_id INTEGER NOT NULL, peptide_name TEXT NOT NULL,PeptideAbundance TEXT NOT NULL,FOREIGN KEY (visit_id) REFERENCES visits (id))')
+
+    print("Created table successfully!")
+    conn.commit()
+    conn.close()
+
+    fill_info()
+
+def patients_l():
+    patients_l = [
+        {"username":"Arwa","password":"qwertyuiop[]", "score":15},
+        {"username":"Rana","password":"asdfghjkl;'", "score":70},
+        {"username":"Eman","password":"zxcvbnm,.", "score":8}
+    ]
+
+    conn = get_db_connection()
+
+    for i in patients_l:
+      conn.execute('INSERT INTO patients (username ,password ,score ) VALUES (?,?,?)',(i['username'],i['password'],i['score']))
+
+    conn.commit()
+    conn.close()
+
+
+def visits_l():
+    visits_l = [
+        {"patient_id":1,"visit_date":"3 March"},
+        {"patient_id":2,"visit_date":"4 June"},
+        {"patient_id":3,"visit_date":"2 May"},
+        {"patient_id":1,"visit_date":"11 May"}
+    ]
+
+    conn = get_db_connection()
+
+    for i in visits_l:
+        conn.execute('INSERT INTO visits (patient_id ,visit_date ) VALUES (?,?)',(i['patient_id'],i['visit_date']))
+    conn.commit()
+
+    conn.close()
+
+def proteins_l():
+    proteins_l = [
+        {"visit_id":1, "protein_name":"O00391","NPX":"11254.3"},
+        {"visit_id":1, "protein_name":"P01717","NPX":"110750"},
+        {"visit_id":1, "protein_name":"P01024","NPX":"3916980"},
+        {"visit_id":1, "protein_name":"P00747","NPX":"43235500"},
+        {"visit_id":1, "protein_name":"P01033","NPX":"732430"},
+        {"visit_id":1, "protein_name":"P00450","NPX":"39585.8"},
+        {"visit_id":1, "protein_name":"O60888","NPX":"1829650"},
+        {"visit_id":1, "protein_name":"O00533","NPX":"24917"},
+        {"visit_id":1, "protein_name":"O00584","NPX":"1613890"},
+        {"visit_id":1, "protein_name":"P01019","NPX":"396955"},#-#
+        {"visit_id":2, "protein_name":"O00391","NPX":"11254.3"},
+        {"visit_id":2, "protein_name":"P01717","NPX":"110750"},
+        {"visit_id":2, "protein_name":"P01024","NPX":"3916980"},
+        {"visit_id":2, "protein_name":"P00747","NPX":"43235500"},
+        {"visit_id":2, "protein_name":"P01033","NPX":"732430"},
+        {"visit_id":2, "protein_name":"P00450","NPX":"39585.8"},
+        {"visit_id":2, "protein_name":"O60888","NPX":"1829650"},
+        {"visit_id":2, "protein_name":"O00533","NPX":"24917"},
+        {"visit_id":2, "protein_name":"O00584","NPX":"1613890"},
+        {"visit_id":2, "protein_name":"P01019","NPX":"396955"},
+        {"visit_id":3, "protein_name":"P01019","NPX":"396955"},#-#
+        {"visit_id":3, "protein_name":"O00391","NPX":"11254.3"},
+        {"visit_id":3, "protein_name":"P01717","NPX":"110750"},
+        {"visit_id":3, "protein_name":"P01024","NPX":"3916980"},
+        {"visit_id":3, "protein_name":"P00747","NPX":"43235500"},
+        {"visit_id":3, "protein_name":"P01033","NPX":"732430"},
+        {"visit_id":3, "protein_name":"P00450","NPX":"39585.8"},
+        {"visit_id":3, "protein_name":"O60888","NPX":"1829650"},
+        {"visit_id":3, "protein_name":"O00533","NPX":"24917"},
+        {"visit_id":3, "protein_name":"O00584","NPX":"1613890"},
+        {"visit_id":4, "protein_name":"P01019","NPX":"396955"},#-#
+        {"visit_id":4, "protein_name":"O00391","NPX":"11254.3"},
+        {"visit_id":4, "protein_name":"P01717","NPX":"110750"},
+        {"visit_id":4, "protein_name":"P01024","NPX":"3916980"},
+        {"visit_id":4, "protein_name":"P00747","NPX":"43235500"},
+        {"visit_id":4, "protein_name":"P01033","NPX":"732430"},
+        {"visit_id":4, "protein_name":"P00450","NPX":"39585.8"},
+        {"visit_id":4, "protein_name":"O60888","NPX":"1829650"},
+        {"visit_id":4, "protein_name":"O00533","NPX":"24917"},
+        {"visit_id":4, "protein_name":"O00584","NPX":"1613890"},
+    ]
+
+    conn = get_db_connection()
+
+    for i in proteins_l:
+        conn.execute('INSERT INTO proteins (visit_id ,protein_name ,NPX) VALUES (?,?,?)',(i['visit_id'],i['protein_name'],i['NPX']))
+
+    conn.commit()
+
+
+def peptides_l():
+    peptides_l = [
+        {"visit_id":1, "peptide_name":"NEQEQPLGQWHLS",  "PeptideAbundance":"11254.3"},
+        {"visit_id":1, "peptide_name":"GNPEPTFSWTK",    "PeptideAbundance":"11254.3"},
+        {"visit_id":1, "peptide_name":"IEIPSSVQQVPTIIK","PeptideAbundance":"110750"},
+        {"visit_id":1, "peptide_name":"SMEQNGPGLEYR",   "PeptideAbundance":"3916980"},
+        {"visit_id":1, "peptide_name":"TLKIENVSYQDKGNYR","PeptideAbundance":"43235500"},
+        {"visit_id":1, "peptide_name":"VIAVNEVGR","PeptideAbundance":"732430"},
+        {"visit_id":1, "peptide_name":"VMTPAVYAPYDVK","PeptideAbundance":"39585.8"},
+        {"visit_id":1, "peptide_name":"VNGSPVDNHPFAGDVVFPR","PeptideAbundance":"1829650"},
+        {"visit_id":1, "peptide_name":"ELDLNSVLLK","PeptideAbundance":"24917"},
+        {"visit_id":1, "peptide_name":"ALPGTPVASSQPR","PeptideAbundance":"1613890"},
+        {"visit_id":2, "peptide_name":"P01019","PeptideAbundance":"396955"},#-#
+        {"visit_id":2, "peptide_name":"NEQEQPLGQWHLS","PeptideAbundance":"11254.3"},
+        {"visit_id":2, "peptide_name":"GNPEPTFSWTK","PeptideAbundance":"11254.3"},
+        {"visit_id":2, "peptide_name":"IEIPSSVQQVPTIIK","PeptideAbundance":"110750"},
+        {"visit_id":2, "peptide_name":"SMEQNGPGLEYR","PeptideAbundance":"3916980"},
+        {"visit_id":2, "peptide_name":"TLKIENVSYQDKGNYR","PeptideAbundance":"43235500"},
+        {"visit_id":2, "peptide_name":"VIAVNEVGR","PeptideAbundance":"732430"},
+        {"visit_id":2, "peptide_name":"VMTPAVYAPYDVK","PeptideAbundance":"39585.8"},
+        {"visit_id":2, "peptide_name":"VNGSPVDNHPFAGDVVFPR","PeptideAbundance":"1829650"},
+        {"visit_id":2, "peptide_name":"ELDLNSVLLK","PeptideAbundance":"24917"},
+        {"visit_id":3, "peptide_name":"ALPGTPVASSQPR","PeptideAbundance":"1613890"},
+        {"visit_id":3, "peptide_name":"NEQEQPLGQWHLS","PeptideAbundance":"11254.3"},
+        {"visit_id":3, "peptide_name":"GNPEPTFSWTK","PeptideAbundance":"11254.3"},
+        {"visit_id":3, "peptide_name":"IEIPSSVQQVPTIIK","PeptideAbundance":"110750"},
+        {"visit_id":3, "peptide_name":"SMEQNGPGLEYR","PeptideAbundance":"3916980"},
+        {"visit_id":3, "peptide_name":"TLKIENVSYQDKGNYR","PeptideAbundance":"43235500"},
+        {"visit_id":3, "peptide_name":"VIAVNEVGR","PeptideAbundance":"732430"},
+        {"visit_id":3, "peptide_name":"VMTPAVYAPYDVK","PeptideAbundance":"39585.8"},
+        {"visit_id":3, "peptide_name":"VNGSPVDNHPFAGDVVFPR","PeptideAbundance":"1829650"},
+        {"visit_id":3, "peptide_name":"ELDLNSVLLK","PeptideAbundance":"24917"},
+        {"visit_id":4, "peptide_name":"ALPGTPVASSQPR","PeptideAbundance":"1613890"},
+        {"visit_id":4, "peptide_name":"NEQEQPLGQWHLS","PeptideAbundance":"11254.3"},
+        {"visit_id":4, "peptide_name":"GNPEPTFSWTK","PeptideAbundance":"11254.3"},
+        {"visit_id":4, "peptide_name":"IEIPSSVQQVPTIIK","PeptideAbundance":"110750"},
+        {"visit_id":4, "peptide_name":"SMEQNGPGLEYR","PeptideAbundance":"3916980"},
+        {"visit_id":4, "peptide_name":"TLKIENVSYQDKGNYR","PeptideAbundance":"43235500"},
+        {"visit_id":4, "peptide_name":"VIAVNEVGR","PeptideAbundance":"732430"},
+        {"visit_id":4, "peptide_name":"VMTPAVYAPYDVK","PeptideAbundance":"39585.8"},
+        {"visit_id":4, "peptide_name":"VNGSPVDNHPFAGDVVFPR","PeptideAbundance":"1829650"},
+        {"visit_id":4, "peptide_name":"ELDLNSVLLK","PeptideAbundance":"24917"}
+        
+    ]
+
+    conn = get_db_connection()
+
+    for i in peptides_l:
+        conn.execute('INSERT INTO peptides (visit_id ,peptide_name, PeptideAbundance ) VALUES (?,?,?)',(i['visit_id'],i['peptide_name'], i['PeptideAbundance']))
+
+    conn.commit()
+
+    
+
+
+def fill_info():
+    conn = get_db_connection()
+    #patients_l()
+    #visits_l()
+    #proteins_l()
+    #peptides_l()
+    j=0
+    j=j+1
+    conn.close()
+
+
+
+
+# Fetch all patients from the database
+def get_patients():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    patients = conn.execute('SELECT * FROM patients').fetchall()
+    conn.close()
+    return patients
+
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    #create_table()
+    fill_info()
+    return render_template( "home.html")
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-        user = next((user for user in users if user["username"] == username), None)
-        
-        if user:
-            if user["password"] == password:
-                if user.get("role") == "admin":
-                    return redirect(url_for("dashboard"))
-                elif user["username"].startswith("doctor"):
-                    return redirect(url_for("doctor", user_id=user["id"]))
-                else:
-                    return redirect(url_for("patient", user_id=user["id"]))
-        
-        error = "اسم المستخدم أو كلمة المرور غير صحيحة."
-        return render_template("login.html", error=error)
+@app.route('/Patients' , methods=['GET'])
+def Patients():
+    Patients = get_patients()
+
+
+    return render_template( "Patients.html",Patients=Patients)
+
+
+@app.route('/patient_info/<int:p_id>')
+def patient_info(p_id):
+    conn = get_db_connection()
     
-    return render_template("login.html")
+    # Fetch patient details
+    patient = conn.execute('SELECT * FROM patients WHERE id = ?', (p_id,)).fetchone()
 
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
+    # Fetch visits and sort them by date in descending order
+    visits = conn.execute('SELECT * FROM visits WHERE patient_id = ? ORDER BY visit_date DESC', (p_id,)).fetchall()
 
-@app.route("/doctor/<int:user_id>")
-def doctor(user_id):
-    doctor = next((user for user in users if user["id"] == user_id), None)
-    patient_username = doctor.get("patient")
-    patient_info = patients_db.get(patient_username)
-    
-    return render_template("doctor.html", doctor=doctor, patient=patient_info)
+    proteins = {}
+    peptides = {}
+    for visit in visits:
+        visit_id = visit['id']
+        visit_date = visit['visit_date']
+        proteins[visit_date] = conn.execute('SELECT * FROM proteins WHERE visit_id = ?', (visit_id,)).fetchall()
+        peptides[visit_date] = conn.execute('SELECT * FROM peptides WHERE visit_id = ?', (visit_id,)).fetchall()
 
-@app.route("/patient/<int:user_id>", methods=["GET", "POST"])
-def patient(user_id):
-    patient = next((user for user in users if user["id"] == user_id), None)
-    doctor_username = patient.get("doctor")
-    
-    if request.method == "POST":
-        return redirect(url_for("home"))
-    
-    return render_template("patient.html", patient=patient, doctor_username=doctor_username)
+    conn.close()
 
-if __name__ == "__main__":
+    # Preprocess data for JavaScript
+    proteins_list = [{'date': date, 'name': protein['name'], 'score': protein['score']} for date, proteins_for_date in proteins.items() for protein in proteins_for_date]
+    peptides_list = [{'date': date, 'name': peptide['name'], 'score': peptide['score']} for date, peptides_for_date in peptides.items() for peptide in peptides_for_date]
+
+    return render_template('patient_info.html', patient=patient, proteins=proteins_list, peptides=peptides_list)
+
+if __name__ == '__main__':
     app.run(debug=True)
+
